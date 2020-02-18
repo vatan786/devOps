1e0e529e2e126599c79d46840782562481a6e145
vatan786 <56758544+vatan786@users.noreply.github.com>
2020-02-18 23:37:42 +0530

    Update pythonwebapp.py

--- pythonwebapp.py
+++ pythonwebapp.py
@@ -1,8 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/"):
+@app.route("/")
+def index():
   return ("Welcome in ...")
 
 if __name__ == "__main__":
-app.run()
+  app.run(debug=True)
+
